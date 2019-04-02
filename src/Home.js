d93b5e3709098f3917e1471d1271b442ed0db742
dpjames <djames@34north.com>
2019-04-02 14:01:18 -0700

    home page and font info

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,25 @@ import React from 'react';
 class Home extends React.Component {
    render(){
       return (
-         <div>
-            Some intro about climbing ugh 
+         <div className="intro">
+            <div>
+            Welcome to my website. Theres not much here yet, but I'll 
+            be developing it more in my free time.
+            As of right now the main funcitonality centers around weather forecasts.
+            Specifically weather forecasts for rock climbing areas and summits. 
+            Hopefully the interface is self explanatory
+            but if not I'd 
+            love feedback. 
+            The arrows let you step through time. Clicking on the map will let you view information about where you clicked, 
+            and show beta about the nearest points of interest. 
+            </div>
+            <br></br>
+            <div>
+            <strong>Only Washington, Oregon, and CA have climbing data right now (this will soon be updated to include the entire west).
+            Weather data is only from Eastern CO border to the west coast. Eventually it will be updated to include the entire US.</strong>
+            </div>
+            <br></br>
+            Shoot me emails at climbingToolbox@gmail.com
          </div>
       );
    }
