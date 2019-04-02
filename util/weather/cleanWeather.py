e2bad5ff899774ebe35f83a772f417f5488dd253
dpjames <djames@34north.com>
2019-04-02 13:26:38 -0700

    I should have ran it first

--- util/weather/cleanWeather.py
+++ util/weather/cleanWeather.py
@@ -15,7 +15,7 @@ def findPrecip(s):
         return 30
     if re.search("little or no (snow|rain)", s) is not None:
         return 10
-    if re.search("(snow|rain)fall" s) is not None:
+    if re.search("(snow|rain)fall", s) is not None:
         return 100
     if re.search("(rain|snow)", s) is not None:
         print "unknown: " + s
