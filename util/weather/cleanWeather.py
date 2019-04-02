1963333f24455e50e5be54dfdf0786794a246f0e
dpjames <djames@34north.com>
2019-04-02 13:25:14 -0700

    script

--- util/weather/cleanWeather.py
+++ util/weather/cleanWeather.py
@@ -13,6 +13,8 @@ def findPrecip(s):
         return 75
     if re.search("chance .* (rain|snow|showers)", s) is not None:
         return 30
+    if re.search("little or no (snow|rain)", s) is not None:
+        return 10
     if re.search("(rain|snow)", s) is not None:
         print "unknown: " + s
         return 100
